0a2a23b066d332392fb2f17ff2f697b0c15129d4
vwaghmode52@gmail.com <rizwan@gmail.com>
2022-11-17 09:52:44 +0530

    first commit

---
+++ code/adding two number in c++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,add;
+    cout<<"/nResult = "<<add;
+    cin>>num1>>num2;
+    add = num1+num2;
+    cout<<"/nResult = "<<add;
+    cout<<end1;
+    retun 0;
+}
